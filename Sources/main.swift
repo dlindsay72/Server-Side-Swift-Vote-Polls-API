d948a9951c4473c4b687ca8447384cf0165519e3
Dan Lindsay <dlindsay72@hotmail.com>
2017-03-02 13:32:31 -0500

    Kitura and CouchDb now talking to each other. First route complete.

--- Sources/main.swift
+++ Sources/main.swift
@@ -2,22 +2,54 @@ import Kitura
 import HeliumLogger
 import LoggerAPI
 import SwiftyJSON
+import CouchDB
 
 HeliumLogger.use()
+let connectionProperties = ConnectionProperties(host: "localhost", port: 5984, secured: false)
+let client = CouchDBClient(connectionProperties: connectionProperties)
+let database = client.database("polls")
 
 let router = Router()
 
 //routes here
 router.get("/polls/list") {
   request, response, next in
-  defer { next() }
 
-  let status = ["status": "ok"]
-  let result = ["result": status]
-  let json = JSON(result)
+  database.retrieveAll(includeDocuments: true) { docs, error in
 
-  response.status(.OK).send(json: json)
+    defer { next() }
 
+    if let error = error {
+      let errorMessage = error.localizedDescription
+      let status = ["status": "error", "message": errorMessage]
+      let result = ["result": status]
+      let json = JSON(result)
+
+      response.status(.OK).send(json: json)
+    } else {
+      // success!
+      let status = ["status": "ok"]
+      var polls = [[String: Any]]()
+
+      if let docs = docs {
+        for document in docs["rows"].arrayValue {
+          var poll = [String: Any]()
+          poll["id"] = document["id"].stringValue
+          poll["title"] = document["doc"]["title"].stringValue
+          poll["option1"] = document["doc"]["option1"].stringValue
+          poll["option2"] = document["doc"]["option2"].stringValue
+          poll["votes1"] = document["doc"]["votes1"].intValue
+          poll["votes2"] = document["doc"]["votes2"].intValue
+
+          polls.append(poll)
+        }
+      }
+      let result: [String: Any] = ["result": status, "polls": polls]
+      let json = JSON(result)
+
+      response.status(.OK).send(json: json)
+    }
+  }
 }
 
 router.post("/polls/create") {
